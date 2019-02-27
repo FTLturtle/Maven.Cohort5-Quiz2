104d2043bf11835b955569304e4e04d29f39fb89
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-02-27 15:36:47 -0500

    Finished SpeedComparator

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -10,20 +10,31 @@ public class Router {
     }
 
     public void add(String path, String controller) {
-
+        treeMap.put(path, controller);
     }
 
     public Integer size() {
-        return null;
+        return treeMap.size();
     }
 
     public String getController(String path) {
-        return null;
+        return treeMap.get(path);
     }
 
     public void update(String path, String studentController) {
+        treeMap.replace(path, studentController);
     }
 
     public void remove(String path) {
+        treeMap.remove(path);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder toStringBuilder = new StringBuilder();
+        for (String s : treeMap.keySet()) {
+            toStringBuilder.append(s).append(" -> ").append(treeMap.get(s)).append('\n');
+        }
+        return toStringBuilder.toString();
     }
 }
--- src/main/java/com/zipcodewilmington/assessment2/part3/SpeedComparator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part3/SpeedComparator.java
@@ -1,4 +1,27 @@
 package com.zipcodewilmington.assessment2.part3;
 
-public class SpeedComparator {
+import org.junit.Assert;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SpeedComparator implements Comparator<Animal> {
+
+    public int compare(Animal animal1, Animal animal2) {
+        return animal2.getSpeed() - animal1.getSpeed();
+    }
+
+    public static void main(String[] args) {
+        Animal horse = (Animal) new Horse();
+        Animal blueJay = (Animal) new BlueJay();
+        Animal redRobin = (Animal) new RedRobin();
+
+        Animal[] animals = {redRobin, horse, blueJay};
+        Comparator comparator = (Comparator) new SpeedComparator();
+        Arrays.sort(animals, comparator);
+
+        for (Animal a : animals) {
+            System.out.println(a.getClass());
+        }
+    }
 }
