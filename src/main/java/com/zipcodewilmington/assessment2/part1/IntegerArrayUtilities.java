3f19e63ecb08c4966f73380e26c8aff652cf7db0
Sunhyun Miller <sunhyunmiller@zipcoders-MacBook-Pro.local>
2019-02-27 13:53:47 -0500

    working on IntegerArrayUtilities

--- src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/IntegerArrayUtilities.java
@@ -2,15 +2,20 @@ package com.zipcodewilmington.assessment2.part1;
 
 public class IntegerArrayUtilities {
     public Boolean hasEvenLength(Integer[] array) {
-        return null;
+        return array.length % 2 == 0;
     }
 
     public Integer[] range(int start, int stop) {
+        int length = stop - start + 1;
+        Integer[] array = new Integer[length];
+
+
+
         return null;
     }
 
     public Integer getSumOfFirstTwo(Integer[] array) {
-        return null;
+        return array[0] + ;
     }
 
     public Integer getProductOfFirstTwo(Integer[] array) {
